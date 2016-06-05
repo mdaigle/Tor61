93f8ae1b1491f2c407337e8efe3aecb01dad5737
Malcolm Daigle <daiglemalcolm@gmail.com>
2016-06-05 14:59:28 -0700

    End node circuit mapping.

--- routerloop.js
+++ routerloop.js
@@ -155,7 +155,7 @@ exports.socketSetup = function(socket, nodeID, createdByUs) {
         case protocol.CREATE:
             console.log("<<< Received CREATE " + circID);
           // add mapping and send created
-        //   mappings.addCircuitMapping(otherNodeID, circID, null, null);
+          mappings.addCircuitMapping(otherNodeID, circID, null, null);
           protocol.sendCreated(socket, circID);
           break;
 
